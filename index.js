550b32606f7b805f0d539da83b2094914ecec795
r1414a <rupeshchincholkar14@gmail.com>
2024-04-16 12:28:20 +0530

    added visitor's count

--- index.js
+++
@@ -1,6 +0,0 @@
-let c = document.getElementById('cursor');
-addEventListener("mousemove",(e) => {
-    c.classList.add(`top-${clientY}`);
-    c.classList.add(`left-${clientX}`);
-    console.log(e);
-})
